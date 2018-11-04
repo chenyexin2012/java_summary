5dbe729b42a27f1481f14f1eeac6a65b5d38aeb7
chenyexin2012 <chenyexin2012@126.com>
2018-11-04 15:49:12 +0800

    add files

--- java_summary_reflection/src/main/java/com/java/summary/proxy/ProxyTest.java
+++ java_summary_reflection/src/main/java/com/java/summary/proxy/ProxyTest.java
@@ -2,7 +2,10 @@ package com.java.summary.proxy;
 
 import com.java.summary.proxy.cglib.ProxyFactory;
 import com.java.summary.proxy.jdk.MyInvocationHandler;
+import net.sf.cglib.proxy.*;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 public class ProxyTest {
 
@@ -44,5 +47,103 @@ public class ProxyTest {
         System.out.println(proxy.mul(1, 1));
         // 除数为0
         System.out.println(proxy.div(1, 0));
+    }
+
+    /**
+     * @see net.sf.cglib.proxy.FixedValue
+     */
+    @Test
+    public void testFieldValue() {
+
+        // 被代理对象
+        com.java.summary.proxy.cglib.Calculator calculator = new com.java.summary.proxy.cglib.Calculator();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(calculator.getClass());
+        enhancer.setCallback(new FixedValue() {
+            @Override
+            public Object loadObject() throws Exception {
+                return 100;
+            }
+        });
+        com.java.summary.proxy.cglib.Calculator proxy = (com.java.summary.proxy.cglib.Calculator) enhancer.create();
+
+        System.out.println(proxy.add(1, 1));
+        System.out.println(proxy.sub(1, 1));
+        System.out.println(proxy.mul(1, 1));
+        System.out.println(proxy.div(1, 1));
+    }
+
+    /**
+     * @see net.sf.cglib.proxy.InvocationHandler
+     */
+    @Test
+    public void testInvocationHandler() {
+
+        // 被代理对象
+        com.java.summary.proxy.cglib.Calculator calculator = new com.java.summary.proxy.cglib.Calculator();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(calculator.getClass());
+        enhancer.setCallback(new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+                Integer result = (Integer) method.invoke(calculator, args);
+
+                return result + 1;
+            }
+        });
+        com.java.summary.proxy.cglib.Calculator proxy = (com.java.summary.proxy.cglib.Calculator) enhancer.create();
+
+        System.out.println(proxy.add(1, 1));
+        System.out.println(proxy.sub(1, 1));
+        System.out.println(proxy.mul(1, 1));
+        System.out.println(proxy.div(1, 1));
+    }
+
+    /**
+     * @see net.sf.cglib.proxy.LazyLoader
+     */
+    @Test
+    public void testLazyLoader() {
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(com.java.summary.proxy.cglib.Calculator.class);
+        enhancer.setCallback(new LazyLoader() {
+            @Override
+            public Object loadObject() throws Exception {
+                System.out.println("通过回调生成对象");
+                return new com.java.summary.proxy.cglib.Calculator();
+            }
+        });
+        com.java.summary.proxy.cglib.Calculator proxy = (com.java.summary.proxy.cglib.Calculator) enhancer.create();
+        System.out.println("在第一次访问对象时，加载对象");
+        // 访问代理对象
+        System.out.println(proxy);
+    }
+
+    /**
+     * @see net.sf.cglib.proxy.Dispatcher
+     */
+    @Test
+    public void testDispatcher() {
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(com.java.summary.proxy.cglib.Calculator.class);
+        enhancer.setCallback(new Dispatcher() {
+            @Override
+            public Object loadObject() throws Exception {
+                System.out.println("通过回调生成对象");
+                return new com.java.summary.proxy.cglib.Calculator();
+            }
+        });
+        com.java.summary.proxy.cglib.Calculator proxy = (com.java.summary.proxy.cglib.Calculator) enhancer.create();
+        System.out.println("第一次访问代理对象");
+        // 访问代理对象
+        System.out.println(proxy);
+        System.out.println("第二次访问代理对象");
+        // 访问代理对象
+        System.out.println(proxy);
     }
 }
