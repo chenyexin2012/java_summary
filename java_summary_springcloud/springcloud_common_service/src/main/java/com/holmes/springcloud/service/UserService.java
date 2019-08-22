d028884b022021d1e6a95d3faf3f50a8d2fb7a7c
chenyexin2012 <chenyexin2012@126.com>
2019-08-22 20:12:50 +0800

    add

---
+++ java_summary_springcloud/springcloud_common_service/src/main/java/com/holmes/springcloud/service/UserService.java
@@ -0,0 +1,10 @@
+package com.holmes.springcloud.service;
+
+import com.holmes.springcloud.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getUserList();
+}
